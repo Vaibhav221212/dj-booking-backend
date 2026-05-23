f974bd2babb06da33b95859ad7be46e4db82b04a
Vaibhav Ashok Pingale <120328228+Vaibhav221212@users.noreply.github.com>
2026-05-23 17:47:46 +0530

    Update user.js

--- src/controller/user.js
+++ src/controller/user.js
@@ -232,7 +232,12 @@ const userLogin = async (req, res) => {
       "rDRiyK6octEQz0yTLZ3o6m8QvtcIUxQkEFRyRc3U3Oa",
     );
 
-    res.cookie("token", token);
+res.cookie("token", token, {
+  httpOnly: false, // frontend read करणार असेल तर false
+  secure: true,
+  sameSite: "None",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
 
     return res.json({
       success: true,
