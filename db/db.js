994c0bff826c1281dbcad8c40ae33a7ccaa81131
Vaibhav Ashok Pingale <120328228+Vaibhav221212@users.noreply.github.com>
2026-05-23 11:47:09 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ import mongoose  from 'mongoose'
 async function dbConnect()
 {
      try{
-         await mongoose.connect("mongodb+srv://vaibhavpingale51_db_user:R1JwkAJLm2t6hrMR@cluster0.wtchmyi.mongodb.net/");
+         await mongoose.connect("mongodb+srv://vaibhavpingale51_db_user:Vaibhav@cluster0.wtchmyi.mongodb.net/?appName=Cluster0");
              console.log("data base cnnected`")
      }
      catch(e)
