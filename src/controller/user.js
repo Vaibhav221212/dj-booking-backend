e45114a79ce9bf3e1dc9ad348053777468214be5
Vaibhav Ashok Pingale <120328228+Vaibhav221212@users.noreply.github.com>
2026-05-23 16:10:17 +0530

    Update user.js

--- src/controller/user.js
+++ src/controller/user.js
@@ -238,7 +238,7 @@ const userLogin = async (req, res) => {
       success: true,
       message: "login sucessfully",
       user: user,
-      role:user.role
+      role:user
     });
   } catch (e) {
     console.log(e);
