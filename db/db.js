51475c27eafc8b31912fa8b80ef006d59d5f1a30
Vaibhav Ashok Pingale <120328228+Vaibhav221212@users.noreply.github.com>
2026-05-23 14:07:37 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 async function dbConnect() {
   try {
     await mongoose.connect(
-      "mongodb+srv://vaibhavpingale51_db_user:Vaibhav@cluster0.wtchmyi.mongodb.net/djapp?retryWrites=true&w=majority&family=4"
+      "mongodb://vaibhavpingale51_db_user:Vaibhav@ac-uag5k6i-shard-00-00.wtchmyi.mongodb.net:27017,ac-uag5k6i-shard-00-01.wtchmyi.mongodb.net:27017,ac-uag5k6i-shard-00-02.wtchmyi.mongodb.net:27017/djapp?ssl=true&replicaSet=atlas-tg0ggq-shard-0&authSource=admin&retryWrites=true&w=majority"
     );
 
     console.log("✅ Database connected");
