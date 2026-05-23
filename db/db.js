82d3b922ce6b6a60fb3f1f16055b907507b1b06b
Vaibhav Ashok Pingale <120328228+Vaibhav221212@users.noreply.github.com>
2026-05-23 14:51:25 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,15 +1,16 @@
-import mongoose from "mongoose";
 
-async function dbConnect() {
-  try {
-    await mongoose.connect(
-      "mongodb://vaibhavpingale51_db_user:Vaibhav@ac-uag5k6i-shard-00-00.wtchmyi.mongodb.net:27017,ac-uag5k6i-shard-00-01.wtchmyi.mongodb.net:27017,ac-uag5k6i-shard-00-02.wtchmyi.mongodb.net:27017/djapp?ssl=true&replicaSet=atlas-tg0ggq-shard-0&authSource=admin&retryWrites=true&w=majority"
-    );
+import mognoose  from 'mongoose'
 
-    console.log("✅ Database connected");
-  } catch (e) {
-    console.log("❌ DB Error:", e);
-  }
+async function dbConnect()
+{
+     try{
+             await mognoose.connect("mongodb://vaibhavpingale51_db_user:Vaibhav@ac-uag5k6i-shard-00-00.wtchmyi.mongodb.net:27017,ac-uag5k6i-shard-00-01.wtchmyi.mongodb.net:27017,ac-uag5k6i-shard-00-02.wtchmyi.mongodb.net:27017/djapp?ssl=true&replicaSet=atlas-tg0ggq-shard-0&authSource=admin&retryWrites=true&w=majority")
+             console.log("data base cnnected`")
+     }
+     catch(e)
+     {
+        console.log(e.message)
+     }
 }
 
 export default dbConnect;
