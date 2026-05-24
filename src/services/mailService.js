3de1d325b6ed549b7a4c3db1708432e9bf3761c4
Vaibhav Ashok Pingale <120328228+Vaibhav221212@users.noreply.github.com>
2026-05-24 23:00:02 +0530

    Update mailService.js

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -8,11 +8,25 @@ console.log(process.env.EMAIL_PASS);
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
-  secure: true,
+  secure: true, // important for 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+});
+
+transporter.verify((error, success) => {
+  if (error) {
+    console.log("VERIFY ERROR:", error);
+  } else {
+    console.log("Mail server ready");
+  }
 });
 
 export default transporter;
