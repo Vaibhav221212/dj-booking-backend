94e9ea30cae9d058a9cec050c405968c3681690c
Vaibhav Ashok Pingale <120328228+Vaibhav221212@users.noreply.github.com>
2026-05-24 22:22:57 +0530

    Update mailService.js

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -4,15 +4,11 @@ import nodemailer from "nodemailer";
 console.log("inside transporter");
 console.log(process.env.EMAIL_USER);
 console.log(process.env.EMAIL_PASS);
-
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
-
 export default transporter;
